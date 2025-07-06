7fbea95110bda6885b887fd1632c9cc2903041d5
GitHub Action <action@github.com>
2025-07-06 18:48:31 +0000

    Bump version to 1.0.19

--- temporal/__init__.py
+++ temporal/__init__.py
@@ -17,7 +17,7 @@ from .plain_year_month import PlainYearMonth
 from .timezone import TimeZone
 from .zoned_date_time import ZonedDateTime
 
-__version__ = "1.0.18"
+__version__ = "1.0.19"
 __all__ = [
     "PlainDate",
     "PlainTime",
